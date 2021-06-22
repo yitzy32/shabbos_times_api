d646e306f7acd21baf8ee975b278649ff27e25d3
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-22 15:45:39 -0400

    dynamic date field

--- app/controllers/api/zmanims_controller.rb
+++ app/controllers/api/zmanims_controller.rb
@@ -1,8 +1,7 @@
 class Api::ZmanimsController < ApplicationController
   def index
-    response = HTTP.get("https://www.hebcal.com/zmanim?cfg=json&zip=11210&date=2021-06-21")
+    response = HTTP.get("https://www.hebcal.com/zmanim?cfg=json&zip=#{params[:zipcode]}&date=#{params[:date]}")
     @data = JSON.parse(response)
-
     zmanim = {}
     zmanim[:date] = @data["date"]
     zmanim[:name] = @data["location"]["name"]
@@ -13,6 +12,11 @@ class Api::ZmanimsController < ApplicationController
     zmanim[:tzeit50min] = @data["times"]["tzeit50min"]
     zmanim[:tzeit72min] = @data["times"]["tzeit72min"]
     ap zmanim
+
+    selected_day = Date.parse(zmanim[:date])
+    plus_1_week = Date.parse(zmanim[:date]) + 7
+
+    convert_to_sring = plus_1_week.strftime("%Y/%m/%d")
     render "index.json.jb"
   end
 end
