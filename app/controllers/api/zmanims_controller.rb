c2a6523dcc1def42dfedf2d2f658d4fb103124cd
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-22 15:05:44 -0400

    save data dynamically to hash

--- app/controllers/api/zmanims_controller.rb
+++ app/controllers/api/zmanims_controller.rb
@@ -2,7 +2,17 @@ class Api::ZmanimsController < ApplicationController
   def index
     response = HTTP.get("https://www.hebcal.com/zmanim?cfg=json&zip=11210&date=2021-06-21")
     @data = JSON.parse(response)
-    ap @data
+
+    zmanim = {}
+    zmanim[:date] = @data["date"]
+    zmanim[:name] = @data["location"]["name"]
+    zmanim[:zip] = @data["location"]["zip"]
+    zmanim[:minchaGedola] = @data["times"]["minchaGedola"]
+    zmanim[:plagHaMincha] = @data["times"]["plagHaMincha"]
+    zmanim[:sunset] = @data["times"]["sunset"]
+    zmanim[:tzeit50min] = @data["times"]["tzeit50min"]
+    zmanim[:tzeit72min] = @data["times"]["tzeit72min"]
+    ap zmanim
     render "index.json.jb"
   end
 end
