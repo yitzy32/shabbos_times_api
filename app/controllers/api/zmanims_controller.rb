1cef03ccd53104f51f7d2f8857a6a43eab6fc677
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-23 22:11:07 -0400

    index for many fridays add @ remove seconds

--- app/controllers/api/zmanims_controller.rb
+++ app/controllers/api/zmanims_controller.rb
@@ -1,25 +1,45 @@
 class Api::ZmanimsController < ApplicationController
+  def index
+    @calendar_of_fridays = []
+    starting_date = Date.parse("march 7 #{Date.today.year}")
+
+    next_friday = starting_date.end_of_week(:saturday)
+    35.times do
+      response = HTTP.get("https://www.hebcal.com/zmanim?cfg=json&zip=#{params[:zipcode]}&date=#{next_friday}")
+      @data = JSON.parse(response)
+
+      @zmanim = {}
+      @zmanim[:date] = Date.parse(@data["date"]).strftime("%A %B %e")
+      @zmanim[:name] = @data["location"]["name"]
+      @zmanim[:mincha_gedola] = DateTime.parse(@data["times"]["minchaGedola"]).strftime("%l:%M %p")
+      @zmanim[:plag_hamincha] = DateTime.parse(@data["times"]["plagHaMincha"]).strftime("%l:%M %p")
+      @zmanim[:sunset] = DateTime.parse(@data["times"]["sunset"]).strftime("%l:%M %p")
+      @zmanim[:tzeis_50_min] = DateTime.parse(@data["times"]["tzeit50min"]).strftime("%l:%M %p")
+      @zmanim[:tzeis_72_min] = DateTime.parse(@data["times"]["tzeit72min"]).strftime("%l:%M %p")
+
+      next_friday += 7
+      @calendar_of_fridays << @zmanim
+    end
+
+    render "index.json.jb"
+  end
+
   def show
     next_friday = Date.today.end_of_week(:saturday)
-    ap next_friday
 
     response = HTTP.get("https://www.hebcal.com/zmanim?cfg=json&zip=#{params[:zipcode]}&date=#{next_friday}")
     @data = JSON.parse(response)
 
-    zmanim = {}
-    zmanim[:date] = Date.parse(@data["date"]).strftime("%A %B %e")
-    zmanim[:name] = @data["location"]["name"]
-    zmanim[:mincha_gedola] = DateTime.parse(@data["times"]["minchaGedola"]).strftime("%l:%M:%S %p")
-    zmanim[:plag_hamincha] = DateTime.parse(@data["times"]["plagHaMincha"]).strftime("%l:%M:%S %p")
-    zmanim[:sunset] = DateTime.parse(@data["times"]["sunset"]).strftime("%l:%M:%S %p")
-    zmanim[:tzeis_50_min] = DateTime.parse(@data["times"]["tzeit50min"]).strftime("%l:%M:%S %p")
-    zmanim[:tzeis_72_min] = DateTime.parse(@data["times"]["tzeit72min"]).strftime("%l:%M:%S %p")
-    ap zmanim
+    @zmanim = {}
+    @zmanim[:date] = Date.parse(@data["date"]).strftime("%A %B %e")
+    @zmanim[:name] = @data["location"]["name"]
+    @zmanim[:mincha_gedola] = DateTime.parse(@data["times"]["minchaGedola"]).strftime("%l:%M %p")
+    @zmanim[:plag_hamincha] = DateTime.parse(@data["times"]["plagHaMincha"]).strftime("%l:%M %p")
+    @zmanim[:sunset] = DateTime.parse(@data["times"]["sunset"]).strftime("%l:%M %p")
+    @zmanim[:tzeis_50_min] = DateTime.parse(@data["times"]["tzeit50min"]).strftime("%l:%M %p")
+    @zmanim[:tzeis_72_min] = DateTime.parse(@data["times"]["tzeit72min"]).strftime("%l:%M %p")
+    ap @zmanim
 
-    selected_day = Date.parse(zmanim[:date])
-    plus_1_week = Date.parse(zmanim[:date]) + 7
-
-    convert_to_sring = plus_1_week.strftime("%Y/%m/%d")
     render "show.json.jb"
   end
 end
